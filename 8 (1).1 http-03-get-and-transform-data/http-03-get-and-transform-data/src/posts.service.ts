f60de14101179f9e6e27af5db0b38310672b64e6
Niket2312 <niket.d@prominentpixel.com>
2021-12-31 19:44:49 +0530

    31-12-21

---
+++ 8 (1).1 http-03-get-and-transform-data/http-03-get-and-transform-data/src/posts.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService{
+    loadedService=[]
+    constructor(private http: HttpClient) {}
+
+    onFetchPosts(){
+    return this.http.get('https://practice-http-58491-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData=>{
+      const postsArray=[];
+      for(let key in responseData){
+        postsArray.push({...responseData[key],id:key})
+      }
+      return postsArray
+    }))
+    
+}
+    getData(data){
+        this.loadedService.push(data);
+        console.log(this.loadedService);
+        
+    }
+    getId(id){
+        console.log(id);
+        
+        return this.loadedService.find(x=>x.id==id)
+    }
+    editId(id){
+        console.log(id);
+        console.log(this.loadedService);
+
+        return this.loadedService=this.loadedService.filter(x=>x.id==id)
+        
+    }
+
+
+}
